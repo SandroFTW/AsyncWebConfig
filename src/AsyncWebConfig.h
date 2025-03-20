b536b368fc791287275138760cc4d59cf0cc21e0
Sandro Hafner <34171311+SandroFTW@users.noreply.github.com>
2025-03-20 12:01:39 +0100

    Update AsyncWebConfig.h

--- src/AsyncWebConfig.h
+++ src/AsyncWebConfig.h
@@ -1,4 +1,6 @@
 /*
+Forked by SandroFTW
+Version 1.0
 
 File WebConfig.h
 Version 1.4
@@ -10,24 +12,12 @@ This library builds a web page with a smart phone friendly form to edit
 a free definable number of configuration parameters.
 The submitted data will bestored in the SPIFFS
 The library works with ESP8266 and ESP32
-
-Dependencies:
-  ESP8266WebServer.h
-  ArduinoJson.h
-
 */
-#ifndef WebConfig_h
-#define WebConfig_h
+#pragma once
 
-#include <Arduino.h>
 #include <ESPAsyncWebServer.h>
-/*
-#if defined(ESP32)
-  #include <WebServer.h>
-#else
-  #include <ESP8266WebServer.h>
-#endif
-*/
+#include <Arduino.h>
+
 //maximum number of parameters
 #define MAXVALUES 30
 
@@ -77,30 +67,17 @@ struct  {
 class AsyncWebConfig {
 public:
   AsyncWebConfig();
+
   //load form descriptions
   void setDescription(String parameter);
   //Add extra descriptions
   void addDescription(String parameter);
-  //function to respond a HTTP request for the form use the filename
-  //to save.
-/*
-#if defined(ESP32)
-  void handleFormRequest(WebServer * server, const char * filename);
-  //function to respond a HTTP request for the form use the default file
-  //to save and restart ESP after saving the new config
-  void handleFormRequest(WebServer * server);
-  //get the index for a value by parameter name
-#else
-  void handleFormRequest(ESP8266WebServer * server, const char * filename);
-  //function to respond a HTTP request for the form use the default file
-  //to save and restart ESP after saving the new config
-  void handleFormRequest(ESP8266WebServer * server);
-  //get the index for a value by parameter name
-#endif
-*/
+
+  //function to respond a HTTP request for the form use the filename to save
   void handleFormRequest(AsyncWebServerRequest *request);
   void handleFormRequest(AsyncWebServerRequest *request, const char * filename);
   int16_t getIndex(const char * name);
+
   //read configuration from file
   boolean readConfig(const char *  filename);
   //read configuration from default file
@@ -113,6 +90,7 @@ public:
   boolean deleteConfig(const char *  filename);
   //delete default configutation file
   boolean deleteConfig();
+
   //get a parameter value by its name
   const String getString(const char * name);
   const char * getValue(const char * name);
@@ -133,20 +111,26 @@ public:
   void setValue(const char*name,String value);
   //set the label for a parameter
   void setLabel(const char * name, const char* label);
+
   //remove all options
   void clearOptions(uint8_t index);
   void clearOptions(const char * name);
+
   //add a new option
   void addOption(uint8_t index, String option);
   void addOption(uint8_t index, String option, String label);
+
   //modify an option
   void setOption(uint8_t index, uint8_t option_index, String option, String label);
   void setOption(char * name, uint8_t option_index, String option, String label);
+
   //get the options count
   uint8_t getOptionCount(uint8_t index);
   uint8_t getOptionCount(char * name);
+
   //set form type to doen cancel
   void setButtons(uint8_t buttons);
+
   //register onSave callback
   void registerOnSave(void (*callback)(String results));
   //register onSave callback
